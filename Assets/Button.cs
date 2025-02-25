38925857ac81b0d67eaedf8c4d1992c673b09fe2
RocherSilencieux <164058459+RocherSilencieux@users.noreply.github.com>
2025-02-25 23:51:47 +0100

    bouton

--- Assets/Button.cs
+++ Assets/Button.cs
@@ -7,13 +7,14 @@ public class Button : MonoBehaviour
 {
 
     [SerializeField] private GameObject button;
+    [SerializeField] private ParticleSystem particle;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision != null && collision.CompareTag("button"))
         {
             Debug.Log("testbutton");
             button.transform.GetChild(0).GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
-
+            particle.Play();
         }
     }
 
